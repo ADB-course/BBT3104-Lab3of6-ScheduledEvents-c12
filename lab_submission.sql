cf1f90edfa3dc41f52d363c812f3a1cc255ef1a5
Fluffycoal <kiptoo.brian@strathmore.edu>
2024-10-01 10:53:33 +0300

    The assignment code

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,11 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE EVENT EVN_average_customer_waiting_time_every_1_hour
+ON SCHEDULE EVERY 1 HOUR
+DO
+BEGIN
+    INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes)
+    SELECT 
+        AVG(TIMESTAMPDIFF(MINUTE, customer_service_ticket_raise_time, CURRENT_TIMESTAMP)) 
+    FROM customer_service_ticket
+    WHERE customer_service_ticket_raise_time >= NOW() - INTERVAL 1 HOUR;
+END;
